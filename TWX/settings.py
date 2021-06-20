2f53c646048ea4114c9a3dd250cd9f2520cf4ce1
aminrohm <amin.rohmatullah@gmail.com>
2021-06-20 12:14:02 +0700

    Update settings.py

--- TWX/settings.py
+++ TWX/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!ux9_(-evqrbgoz1f(h!aa8y5bpz^qhe@rbgaos2vui5#0hl38'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
